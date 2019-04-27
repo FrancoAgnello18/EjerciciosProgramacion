5e34b03c7e06407cdf784c88eae71485f5d0fa57
Franco Agnello <49595320+FrancoAgnello18@users.noreply.github.com>
2019-04-27 20:03:44 -0300

    Laboratorio 2

---
+++ Laboratorio2.java
@@ -0,0 +1,19 @@
+package laboratorio2;
+
+public class Laboratorio2 {
+
+    public static void main(String[] args) { // Punto 1
+
+        Entrenador entrenador1 = new Entrenador();// Punto 5.1
+        entrenador1.setEntrenador("Francisco", "Tenis");
+
+        entrenador1.indicarEntrenamiento(); // Punto 5.2
+        System.out.println("La variable entrenador1: " + entrenador1 + "\n");// Punto 5.3
+
+        Entrenador entrenador2 = new Entrenador("Franco", "Basquet"); // Punto 5.4       
+        entrenador2.indicarEntrenamiento(); // Punto 5.5
+        System.out.println("La variable entrenador2: " + entrenador2); // Punto 5.6       
+
+    }
+
+}
